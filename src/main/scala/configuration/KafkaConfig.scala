40e028431ebcf873798926d6ef5b7850bb4c68c4
Mark Keinhörster <mark.keinhoerster@gmail.com>
2016-01-31 14:21:31 +0100

    KafkaConfig now searches for all entries itself. No double-definition of keys needed anymore

--- src/main/scala/configuration/KafkaConfig.scala
+++ src/main/scala/configuration/KafkaConfig.scala
@@ -1,38 +1,20 @@
 package configuration
 
 import com.typesafe.config.ConfigFactory
-
+import scala.collection.JavaConversions._
 import scala.collection.breakOut
 
 class KafkaConfig{
-  def configKeys : Seq[String] = Seq()
-  lazy val config = ConfigFactory.load()
-  lazy val asMap : Map[String, Object] = configKeys.filter(key => config.hasPath(key))
-    .map(key => (key, config.getString(key)))(breakOut)
+  def config = ConfigFactory.load()
+  lazy val asMap : Map[String, Object] = config.entrySet()
+    .map(entry => (entry.getKey(), config.getString(entry.getKey())))(breakOut)
+
 }
 
 trait KafkaConsumerConfig extends KafkaConfig{
-  val groupId = s"group.id"
-  val zookeeperConnect = s"zookeeper.connect"
-
-  abstract override def configKeys = Seq[String](
-    groupId,
-    zookeeperConnect)++super.configKeys
+  abstract override def config = super.config.getConfig("consumer")
 }
 
 trait KafkaProducerConfig extends KafkaConfig{
-  val brokers = s"metadata.broker.list"
-  val serializer = s"serializer.class"
-  val keyserializer = s"key.serializer"
-  val valueserializer = s"value.serializer"
-  val producertype = s"producer.type"
-  val bootstrap = s"bootstrap.servers"
-
-  abstract override def configKeys = Seq[String](
-      brokers,
-      serializer,
-      producertype,
-      bootstrap,
-      keyserializer,
-      valueserializer) ++ super.configKeys
+  abstract override def config = super.config.getConfig("producer")
 }
